dd3866ee61a7403a66f608f9bbbabfcd6c59f681
Your NameTharindi <tharamt6@gmail.com>
2025-09-20 20:12:05 +0530

    Refactor handleReceiveMessage to integrate GeminiService for reply generation and improve message handling logic

--- src/service/webhook.service.ts
+++ src/service/webhook.service.ts
@@ -2,10 +2,13 @@ import { Request, Response } from "express";
 import { webhookMessageDto, WebhookVerificationDto, WebhookVerificationResponseDto } from "../dto/webhookVerification.dto";
 import { APP_CONFIG } from "../config/app.config";
 import { MessageService } from "./message.service";
+ import { GeminiService } from "./gemini.service";
+ 
 export class WebhookService {
 
     private static instance: WebhookService;
     private messageService: MessageService;
+    private geminiService: GeminiService;
 
     public static getInstance(): WebhookService {
         if (!WebhookService.instance) {
@@ -17,6 +20,7 @@ export class WebhookService {
 
     private constructor() {
         this.messageService = MessageService.getInstance();
+        this.geminiService = GeminiService.getInstance();
 
 
     }
@@ -43,23 +47,53 @@ export class WebhookService {
          };
     }
 
-    public async handleWebhookMessage(data: webhookMessageDto):Promise<boolean>{
-          const message = data.entry[0].changes[0].value.messages[0].text.body;
-        const phoneNumber = data.entry[0].changes[0].value.contacts[0].wa_id;
-        const type = data.entry[0].changes[0].value.messages[0].type;
-        const name = data.entry[0].changes[0].value.contacts[0].profile.name;
+    public async handleReceiveMessage(data: webhookMessageDto):Promise<boolean>{
 
-        const replyMessage = `Hello ${name}, we have received your message`;
-        const isReplied = await this.messageService.sendMessage(phoneNumber, replyMessage);
-
-        if(isReplied){
+        
+        const status = data.entry[0].changes[0].value.statuses;
+        console.log(JSON.stringify(status));
+        if(status !== undefined && status.length>0){
+            console.log('status: ', status[0].status);
             return true;
         }
 
-        return false;
-    }
-        }
+        try{
 
+            const message = data.entry[0].changes[0].value.messages[0].text?.body;
+            if(message === undefined){
+                console.log('message is undefined');
+                console.log(JSON.stringify(data));
+                return true;
+            }
+       
+             
+             const phoneNumber = data.entry[0].changes[0].value.contacts[0].wa_id;
+             const name = data.entry[0].changes[0].value.contacts[0].profile.name;
+
+
+        //const replyMessage = `Hello ${name}, Your Message Received`;
+
+             const replyMessage = await this.geminiService.generateReply(message);
+
+             const isReplied = await this.messageService.sendMessage(phoneNumber, replyMessage);
+             
+        
+             
+
+                if(isReplied){
+                return true;
+            }
+        }
+         catch(error:any){
+            console.log(error.message);
+            return true;
+         }
+        
+         return false;
+    }
+}
+
+    
 
 //singleton pattern ekak use krl hdpu clz ekakin object ekk hadenne
 //system eke life time ektma eka paarai
